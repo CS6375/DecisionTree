cc12e1e4d78070de5c13e018753e8fe684add560
Hanlin He <hanling.he@gmail.com>
2017-09-25 19:34:14 -0500

    Add jar info.

--- src/test/java/cs6375/github/io/DecisionTreeNodeTest.java
+++ src/test/java/cs6375/github/io/DecisionTreeNodeTest.java
@@ -6,11 +6,10 @@ public class DecisionTreeNodeTest {
 
     @Test
     public void DecisionTreeNode() throws Exception {
-        String trainingfile = this.getClass().getClassLoader().getResource("data_sets1/training_set.csv").getFile();
+        String trainingfile = this.getClass().getClassLoader().getResource("data_sets2/training_set.csv").getFile();
         TrainingDataSet ds = new TrainingDataSet(trainingfile);
 
         DecisionTreeNode dt = new DecisionTreeNode(ds);
-//        System.out.println(dt);
-        System.out.println(dt.getRight().getLabel());
+        System.out.println(dt);
     }
 }
